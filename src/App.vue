3a3fbf2247d06750ed7801e41c879acfc485643e
Junaid Ali <engrjunaidali0@gmail.com>
2022-06-29 21:50:31 +0500

    font changed to Montserrat

--- src/App.vue
+++ src/App.vue
@@ -24,4 +24,8 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;1,100;1,300&display=swap');
+.app{
+  font-family: 'Montserrat', sans-serif !important;
+}
 </style>
--- src/components/navBar/verticalNavbar.vue
+++ src/components/navBar/verticalNavbar.vue
@@ -60,6 +60,10 @@ export default {
 
 </script>
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;1,100;1,300&display=swap');
+.navbar{
+  font-family: 'Montserrat', sans-serif;
+}
 .sticky {
   position: fixed;
   top: 1px;
